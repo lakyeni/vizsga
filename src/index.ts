9568841263d29dc5f1aa50941bc55d23b9a6d778
Laky Enikő <laky.eni@gmail.com>
2026-09-14 21:35:33 +0200

    update index

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,22 @@ const participantManager = new ParticipantManager();
 
 // Esemény létrehozása
 const event1 = new Event(1, "Ádám születésnapja", new Date('2026-10-10'), "Budapest, Ferenciek tere", new EventType("Születésnap"), "Kovács Péter");
+//Esemény hozzáadása 
 eventManager.addEvent(event1);
 
 const event2 = new Event(2, "Klau&Erik esküvője", new Date('2026-11-15'), "Etyek, Nap utca 22", new EventType("Esküvő"), "Kiss Fanni");
 eventManager.addEvent(event2);
 
+//esemény frissítése 
+eventManager.updateEvent(event2, new Event(2, "Klau&Erik esküvője", new Date('2027-05-15'), "Etyek, Nap utca 22", new EventType("Esküvő"), "Kiss Fanni"));
+
+console.log(`Esemény frissítve: ${event2.name}, új dátum: ${event2.date.toDateString()}`);
+
 const event3 = new Event(3, "Tisza-fesztivál", new Date('2027-08-20'), "Szeged, Dóm tér", new EventType("Fesztivál"), "Nagy László");
+
 eventManager.addEvent(event3);
 
-// Résztvevők hozzáadása az eseményekhez
+// Résztvevők hozzáadása az egyes eseményekhez
 participantManager.addParticipant(event1, "Cserepes Virág", "cserepes.virag@example.com");
 participantManager.addParticipant(event1, "Kiss Fanni", "kiss.fanni@example.com");
 participantManager.addParticipant(event1, "Nagy László", "nagy.laszlo@example.com");
@@ -28,8 +35,14 @@ participantManager.addParticipant(event3, "Kovács Péter", "kovacs.peter@exampl
 participantManager.addParticipant(event3, "Kiss Fanni", "kiss.fanni@example.com");
 participantManager.addParticipant(event3, "Nagy László", "nagy.laszlo@example.com");
 
+//Résztvevő törlése bizonyos eseményből
+participantManager.removeParticipant(event1, 2); 
+console.log(`Résztvevő törölve az eseményből: ${event1.name}`);
+
 // Események listázása
 console.log("Összes esemény:");
 eventManager.listEvents().forEach(event => {
     console.log(`ID: ${event.getId()}, Név: ${event.name}, Dátum: ${event.date.toDateString()}, Helyszín: ${event.location}, Típus: ${event.eventType.name}, Szervező: ${event.organizer}`);
 });
+
+console.log("a program vége");
