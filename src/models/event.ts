5949e0b0c8171b4ae67a1ea51f55f306590c9d94
Laky Enikő <laky.eni@gmail.com>
2026-09-14 19:16:00 +0200

    Update Event model

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,8 +1,32 @@
 import { EventType } from "./eventType";
+import { Participant } from "./participant";
 
-export interface Event {
+export interface IEvent {
     id: number;
     name: string;
-    organizer: string;
+    date: Date;
+    location: string;
     eventType: EventType;
+    participants: Participant[];
+    organizer: string;
+}
+
+export class Event implements IEvent {
+    id: number;
+    participants: Participant[] = [];
+
+    constructor(
+        id: number,
+        public name: string,
+        public date: Date,
+        public location: string,
+        public eventType: EventType,
+        public organizer: string
+    ) {
+        this.id = id;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
 }
